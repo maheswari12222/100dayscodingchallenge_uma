3e27354cb46fca8fdf987237dcbff619fdb284ba
Uma Maheswari <123447867+maheswari12222@users.noreply.github.com>
2024-03-06 21:39:48 +0530

    Add files via upload

---
+++ Week-5/day29/Day29challenge.py
@@ -0,0 +1,9 @@
+# Prompt the user for their name
+name = input("Please enter your name: ")
+
+# Greet the user
+print(f"Hello, {name}!")
+
+# Calculate and display the length of their name
+name_length = len(name)
+print(f"Your name has {name_length} characters.")

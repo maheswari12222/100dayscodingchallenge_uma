e8110b8f2c4a34267c0784ab1656130abfed533b
Uma Maheswari <123447867+maheswari12222@users.noreply.github.com>
2024-03-06 22:26:53 +0530

    Add files via upload

---
+++ Week-5/Day30/Day30challenge.py
@@ -0,0 +1,17 @@
+# Declare variables
+age = 25
+height = 1.75
+first_name = "uma"
+
+# Concatenate first name with a greeting message and print the result
+greeting_message = "Hello, " + first_name + "!"
+print(greeting_message)
+
+# Calculate the square of age and print the result
+age_square = age ** 2
+print("The square of your age is:", age_square)
+
+# Calculate BMI and print the result
+weight = 70
+bmi = weight / (height ** 2)
+print("Your BMI is:", bmi)
